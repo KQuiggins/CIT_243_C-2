acbe166198564d2f52c56a3c1dcbc0679c8fd5ab
KQuiggins <kashfamilyvalues@gmail.com>
2022-08-15 18:57:24 -0400

    initial commit

---
+++ DemoLogo/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+class DemoLogo
+{
+    static void Main()
+    {
+        Write("Our company is ");
+        DisplayCompanyLogo();
+    }
+    private static void DisplayCompanyLogo()
+    {
+        WriteLine("See Sharp Optical");
+        WriteLine("We Prize your eyes");
+    }
+}
