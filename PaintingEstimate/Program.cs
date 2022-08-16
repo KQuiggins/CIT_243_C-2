f7e0ab04c7f933d2af4fd998dbd32b6f6469ea24
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-08-16 15:00:37 -0400

    added PaintingEstimate program

---
+++ PaintingEstimate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+using System.Globalization;
+class PaintingEstimate
+{
+    static void Main()
+    {
+        Write("What is the length of the room? ");
+        int length = int.Parse(ReadLine());
+        Write("What is the width of the room? ");
+        int width = int.Parse(ReadLine());
+
+        // Call Method
+        double totalPrice = ComputeCost(length, width);
+
+        WriteLine("The total price of the room is {0}", totalPrice.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+
+    }
+
+    public static double ComputeCost(int length, int width)
+    {
+        // Price per square foot
+        int price = 6;
+        // Ceiling
+        int ceiling = 9;
+
+        int totalSquare = width * ceiling * 2 + length * ceiling * 2;
+
+        return price * totalSquare;
+    }
+}
