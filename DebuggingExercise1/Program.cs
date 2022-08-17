56e517a4ce999683be933ea9f3d9afa5d6d83fa1
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-08-17 15:13:10 -0400

    added debugging exercise 1

---
+++ DebuggingExercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Uses DisplayWebAddress method three times
+using static System.Console;
+class DebugSeven1
+{
+   static void Main()
+   {
+      DisplayWebAddress(); // method call had no parentheses & Address was missing an s
+      WriteLine("Shop at Shopper's World"); // L was lowercase L
+      DisplayWebAddress(); // method call had no parentheses
+      WriteLine("The best bargains from around the world");
+      DisplayWebAddress(); // method call had no parentheses
+   }
+   public static void DisplayWebAddress()
+   {
+      WriteLine("------------------------------");
+      WriteLine("Visit us on the web at:");
+      WriteLine("www.shoppersworldbargains.com");
+      WriteLine("******************************");
+   }
+}
