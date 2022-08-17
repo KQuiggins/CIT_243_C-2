97edc75e3c7a24d6879dc8d578cd8a389102d279
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-08-17 13:38:23 -0400

    added ConvertMilesToKilometers program

---
+++ ConvertMilesToKilometers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+class ConvertMilesToKilometers
+{
+   static void Main()
+   {
+        Write("What is the distance in miles? ");
+        double miles = Convert.ToDouble(ReadLine());
+        double kilometers = ConvertToKilometers(miles);
+        WriteLine("The distance in kilometers is {0}", kilometers);
+   }
+   public static double ConvertToKilometers(double miles)
+   {
+        double kilometers = miles * 1.60934;
+        return kilometers; 
+
+   }
+}
