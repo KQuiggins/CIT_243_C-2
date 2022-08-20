4f2d494e88fc0310c763eda72f8e6069334a747a
KQuiggins <kashfamilyvalues@gmail.com>
2022-08-20 07:48:55 -0400

    added practiceCode folder to project

---
+++ practiceCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+class Sentence
+{
+    string[] words = "The quick brown fox.".Split();
+
+    public string this[int wordNum]
+    {
+        get { return words[wordNum]; }
+        set { words[wordNum] = value; }
+    }
+
+    static void Main()
+    {
+        Sentence s = new Sentence();
+        WriteLine(s[3]);
+        s[3] = "kangaroo";
+        WriteLine(s[3]);
+    }
+}
