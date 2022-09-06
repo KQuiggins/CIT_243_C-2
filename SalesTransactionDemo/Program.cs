8c63afed0f99ad1dc1c842104f001f221ea15548
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-09-06 15:47:58 -0400

    worked on SalesTransactionDemo

--- SalesTransactionDemo/Program.cs
+++ SalesTransactionDemo/Program.cs
@@ -3,69 +3,73 @@ using static System.Console;
 using System.Globalization;
 class SalesTransactionDemo
 {
-    public string Name { get; set; }
-    public double salesAmount { get; set; }
-    public double commission { get; set; }
-    readonly double RATE;
 
-    public static SalesTransactionDemo operator +(SalesTransactionDemo s1, SalesTransactionDemo s2)
-    {
-        SalesTransactionDemo s3 = new SalesTransactionDemo();
-        s3.salesAmount = s1.salesAmount + s2.salesAmount;
-        
-        return s3;
-    }
 
+  
     
+   static void Main()
+   {
+     SalesTransaction sale1 = new SalesTransaction("Joe", 200.50, .2033);
+    SalesTransaction sale2 = new SalesTransaction("Ellen", 15.99);
+    SalesTransaction sale3 = sale1 + sale2;
+    WriteLine("{0} {1}", sale3.Name, sale3.SalesAmount);
+        
+   }
+   public static void Display(SalesTransaction s)
+   {
+         WriteLine("{0} {1}", s.Name, s.SalesAmount, );
+         
+   }
 
+   public static void DisplayTotal(SalesTransaction s)
+   {
+        
+   }
+}
 
-
-    public SalesTransactionDemo(string name, double salesAmount, double rate)
-    {
-        Name = name;
-        this.salesAmount = salesAmount;
-        RATE = rate;
-        commission = salesAmount * RATE;
-    }
-
-    public SalesTransactionDemo(string name, double salesAmount)
-    {
-        Name = name;
-        this.salesAmount = salesAmount;
-        RATE = 0;
-    }
-
-    public SalesTransactionDemo(string name)
-    {
-        Name = name;
-        salesAmount = 0;
-        RATE = 0;
-        commission = 0;
-    }
-
+class SalesTransaction
+{
+    public double salesAmount {get; set;}
+    public string Name {get; set;}
+    public double Commission {get; set;}
+    public double RATE {get; set;}
+    
     public double getRate()
     {
         return RATE;
     }
-
-
-   static void Main()
-   {
-        SalesTransactionDemo s1 = new SalesTransactionDemo("John", 1000, 0.05);
-   }
-   public static void Display(SalesTransaction s)
-   {
-        WriteLine("Name: {0}", s.Name);
-        WriteLine("Sales Amount: {0}", s.salesAmount.ToString("C", CultureInfo.CurrentCulture));
-        WriteLine("Commission: {0}", s.commission.ToString("C", CultureInfo.CurrentCulture));
-        WriteLine("Rate: {0}", s.getRate().ToString("P", CultureInfo.CurrentCulture));
-   }
-   public static void DisplayTotal(SalesTransaction s)
-   {
-        WriteLine("Total Sales Amount: {0}", s.salesAmount.ToString("C", CultureInfo.CurrentCulture));
-        WriteLine("Total Commission: {0}", s.commission.ToString("C", CultureInfo.CurrentCulture));
-   }
+    
+    public static SalesTransaction operator+(SalesTransaction st1, SalesTransaction st2)
+    {
+        double newSalesAmount = st1.SalesAmount + st2.SalesAmount;
+        return (new SalesTransaction("total", newSalesAmount));
+    }
+    
+    public SalesTransaction(string name, double salesAmount, double rate)
+    {
+        Name = name;
+        SalesAmount = salesAmount;
+        RATE = rate;
+        Commission = salesAmount * rate;
+    }
+    
+    
+     public SalesTransaction(string name, double salesAmount)
+    {
+        Name = name;
+        SalesAmount = salesAmount;
+        RATE = 0;
+    }
+    
+    public SalesTransaction(string name)
+    {
+        Name = name;
+        SalesAmount = 0;
+        RATE = 0;
+        Commission = 0;
+    }
 }
+
 
 /* Create an application named that declares several SalesTransaction objects and displays their values and their sum  */
 
