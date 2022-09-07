083adf41fbc96356e4e171de9be7681cc7b5f423
KQuiggins <kashfamilyvalues@gmail.com>
2022-09-07 10:08:12 -0400

    made changes to RelativeList program

--- RelativeList/Program.cs
+++ RelativeList/Program.cs
@@ -4,12 +4,34 @@ class RelativesList
 {
    static void Main()
    {
-      RelativesList[] relativeArray = new RelativesList[12];
+      Relative[] relArr = new Relative[12];
+      int x;
+        for (x = 0; x < relArr.Length; x++)
+        {
+            WriteLine("Relative {0};", x+1);
+            relArr[x] = new Relative();
+            WriteLine("Relative's Name: ");
+            relArr[x].Name = ReadLine();
+            WriteLine("Relationship to you:");
+            relArr[x].Relationship = ReadLine();
+            WriteLine("Birth Day: ");
+            relArr[x].Day = int.Parse(ReadLine());
+            WriteLine("Birth Month: ");
+            relArr[x].Month = int.Parse(ReadLine());
+            WriteLine("Birth Year: ");
+            relArr[x].Year = int.Parse(ReadLine());
+        }
+        Array.Sort(relArr);
+        WriteLine("Sorted Relative's List: ");
+        for (x = 0; x < relArr.Length; x++)
+        {
+            WriteLine("{0,-25}{1,-25}{2}/{3}/{4}", relArr[x].Name, relArr[x].Relationship, relArr[x].Day, relArr[x].Month, relArr[x].Year);
+        }
 
    }
 }
 
-class Relative
+class Relative : IComparable
 {
    public string Name {get; set;}
    public string Relationship {get; set;}
