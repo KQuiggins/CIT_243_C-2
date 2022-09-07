7f2888447af6bc3010ce9a8b29fe86da880539e5
Sebastian Felling <sebastian@felling.de>
2022-09-07 19:45:12 +0200

    reviewed project

--- SalesTransactionDemo/Program.cs
+++ SalesTransactionDemo/Program.cs
@@ -1,80 +1,8 @@
 ﻿using System;
 using static System.Console;
 using System.Globalization;
-class SalesTransactionDemo
-{
 
-
-  
-    
-   static void Main()
-   {
-        SalesTransaction sale1 = new SalesTransaction("Joe", 200.50, 20.00);
-        SalesTransaction sale2 = new SalesTransaction("Ellen", 15.99);
-        SalesTransaction sale3 = new SalesTransaction("Sue");
-        SalesTransaction sale4;
-        sale4 = sale1 + sale2;
-        WriteLine(sale1.salesAmount.ToString("C", CultureInfo.GetCultureInfo("en-US")));
-        
-   }
-   public static void Display(SalesTransaction s)
-   {
-         WriteLine($"{s.Name} {s.salesAmount.ToString("C", CultureInfo.CurrentCulture)} {s.commission.ToString("C", CultureInfo.CurrentCulture)} {s.getRate().ToString("P", CultureInfo.CurrentCulture)}");
-         
-   }
-
-   public static void DisplayTotal(SalesTransaction s)
-   {
-        WriteLine($"Total Sales: {s.salesAmount.ToString("C", CultureInfo.CurrentCulture)}");
-   }
-}
-
-class SalesTransaction
-{
-    public double salesAmount {get; set;}
-    public string Name {get; set;}
-    public double commission {get; set;}
-    readonly double RATE;
-    
-    public double getRate()
-    {
-        return RATE;
-    }
-    
-    public static SalesTransaction operator+(SalesTransaction st1, SalesTransaction st2)
-    
-    {
-        
-        double newSalesAmount;
-        newSalesAmount = st1.salesAmount + st2.salesAmount;
-        return (new SalesTransaction(newSalesAmount));
-    }
-    
-    public SalesTransaction(string name, double salesAmount, double rate)
-    {
-        Name = name;
-        this.salesAmount = salesAmount;
-        RATE = rate;
-        commission = salesAmount * rate;
-    }
-    
-    
-     public SalesTransaction(string name, double salesAmount)
-    {
-        Name = name;
-        this.salesAmount = salesAmount;
-        RATE = 0;
-    }
-    
-    public SalesTransaction(string name)
-    {
-        Name = name;
-        salesAmount = 0;
-        RATE = 0;
-        commission = 0;
-    }
-}
-
+namespace SalesTransactionDemo;
 
 /* Create an application named that declares several SalesTransaction objects and displays their values and their sum  */
 
@@ -94,3 +22,33 @@ The second constructor accepts a name and sales amount, but sets the commission 
 The third constructor accepts a name and sets all the other fields to 0.
 
 An overloaded + operator adds the sales values for two SalesTransaction objects and returns a new SalesTransaction object. */
+
+class Program
+{
+    public static void Main()
+    {
+        CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+        var sales = new List<SalesTransaction>()
+        {
+            new SalesTransaction("Joe", 200.50, 0.2),
+            new SalesTransaction("Ellen", 15.99),
+            new SalesTransaction("Sue")
+        };
+        
+        sales.Add(sales[0] + sales[1]);
+
+        foreach (var sale in sales)
+        {
+            WriteLine(sale);
+        }
+
+        /*
+        Output:
+        Joe $200.50 $40.10 20.000%
+        Ellen $15.99 $0.00 0.000%
+        Sue $0.00 $0.00 0.000%
+        Joe+Ellen $216.49 $0.00 0.000%
+        */
+    }
+}
---
+++ SalesTransactionDemo/SalesTransaction.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+
+namespace SalesTransactionDemo;
+
+public class SalesTransaction
+{
+    public SalesTransaction(string name, double salesAmount, double rate)
+    {
+        Name = name;
+        SalesAmount = salesAmount;
+        Rate = rate;
+        Commission = salesAmount * rate;
+    }
+    /// <summary>
+    /// The : this(...) part is called constructor chaining. Effectively, it chains
+    /// calls to different (overloaded) constructors.
+    /// An alternative to overloading constructors could be defaulting/optionsl parameters.
+    /// </summary>
+    public SalesTransaction(string name, double salesAmount)
+        : this(name, salesAmount, 0)
+    { }
+    public SalesTransaction(string name): this(name, 0, 0)
+    { }
+
+    /// <summary>
+    /// Just using a getter and leaving out the setter means: this property is
+    /// readonly. It can only be set during initialization (from inside a constructor).
+    /// </summary>
+    public string Name { get; }
+    public double SalesAmount { get; }
+    /// <summary>
+    /// This requirement is not following good practices:
+    /// Monetary values should use the Decimal type.
+    /// See: https://msdn.microsoft.com/en-us/library/364x0z75(v=vs.140).aspx
+    /// </summary>
+    public double Commission { get; }
+    /// <summary>
+    /// We don't use accessors of the type getRate() in C#.
+    /// Properties (getters/setters) are first-class citizens, hence we use them.
+    /// Also, we use PascalCasing, not camelCasing.
+    /// </summary>
+    public double Rate { get; }
+
+    public override string ToString()
+    {
+        // For the $"" notation, see: String Interpolation
+        // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
+        return $"{Name}\t\t\t{SalesAmount:C}\t\t\t{Commission:C}\t\t\t{Rate:P}";
+    }
+    public static SalesTransaction operator +(SalesTransaction st1, SalesTransaction st2)
+    {
+        var combinedNames = $"{st1.Name}+{st2.Name}";
+        var totalSalesAmount = st1.SalesAmount + st2.SalesAmount;
+
+        return new SalesTransaction(combinedNames, totalSalesAmount);
+    }
+}
