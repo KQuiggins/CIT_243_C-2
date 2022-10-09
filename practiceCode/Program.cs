1a94ec5110e7b453394d8bc23f2937a55801c6cf
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-10-09 15:55:33 -0400

    added practiceCode file

--- practiceCode/Program.cs
+++ practiceCode/Program.cs
@@ -3,19 +3,32 @@ using System.Reflection;
 using System.Collections.Generic;
 
 
-class PracticeCode
-{
-    public int IDNum {get; set;}
-    public string Name {get; set;}
-    static void Main()
-    {
-        Name = "Kenny";
-        IDNum = 1;
-        WriteLine(ToString());
-    }
 
-    public override string ToString()
-    {
-        return($"{Name} : {IDNum}");
-    }
+
+int num = 13, denom = 0, result;
+int[] array = {22, 13, 44};
+bool isGoodItem = false;
+WriteLine(!isGoodItem);
+
+
+try
+{
+    result = num / denom;
+    result = array[num];
 }
+
+catch(DivideByZeroException error)
+{
+    WriteLine("In the first catch block: ");
+    WriteLine(error.Message);
+
+}
+catch(IndexOutOfRangeException error)
+{
+    WriteLine("In the second catch block: ");
+    WriteLine(error.Message);
+    WriteLine(error.StackTrace);
+
+}
+
+    
