776300a1ffcdfaaebeae5fbb583fc9cd47f8c69b
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-10-10 14:42:37 -0400

    completed SwimmingWaterTemperature program

--- SwimmingWaterTemperature/Program.cs
+++ SwimmingWaterTemperature/Program.cs
@@ -2,53 +2,59 @@
 using static System.Console;
 class SwimmingWaterTemperature
 {
-   static void Main()
-   {
-       
-       Write("\nPlease enter the water temperature in degrees Fahrenheit: ");
-       while (true)
+    static void Main()
+    {
+
+        WriteLine("Please enter the water temperature in degrees Fahrenheit: ");
+        while (true)
         {
-            
-            
+
+
             try
             {
-                int temp = (ReadLine());
-                if (temp == 999) {break;}
+                int temp = Convert.ToInt32(ReadLine());
+
+                if (temp == 999) { break; }
+
                 else if (CheckComfort(temp) == true)
                 {  // Call the method to check if it's true/false (comfortable for swimming) 
-                    Write(temp + " degrees is comfortable for swimming.\n");
+                    WriteLine($"{temp} degrees is comfortable for swimming.");
                 }
                 else if (CheckComfort(temp) == false)
                 {
-                    Write(temp + " degrees is not comfortable for swimming.\n");
+                    WriteLine($"{temp} degrees is not comfortable for swimming.");
                 }
-                
-                Write("Please enter another temperature or 999 to quit: ");
+
+                WriteLine("Please enter another temperature or 999 to quit: ");
 
             }
-            catch (ApplicationException ex)
+            catch (ApplicationException)
             {
                 WriteLine("Value does not fall within the expected range.");
-                WriteLine(ex.ToString());
+                WriteLine("Enter another temperature or 999 to quit: ");
             }
         }
-   }
-    
-    public static Boolean CheckComfort(int temp) {
+    }
 
-        
-        
+    public static Boolean CheckComfort(int temp)
+    {
+
+
+
         if (temp >= 70 && temp <= 85)
         {
             return true;
         }
-        else if(temp >= 32 && temp <= 212)
+        else if (temp >= 32 && temp <= 212)
         {
             return false;
         }
-        
-       
-        
+        else{
+            throw new ApplicationException();
+        }
+
+
+
 
     }
 
