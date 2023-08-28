60692eefd0d0397c317a05893c3a8b12345600c3
KQuiggins <kashfamilyvalues@gmail.com>
2023-08-28 15:40:25 -0400

    Added Microsoft Starter

---
+++ InputMethodDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+public class InputMethodDemoTwo
+{
+    public static void Main()
+    {
+        int first, second;
+
+        InputMethod(out first, out second);
+        WriteLine($"After InputMethod first is {first} and second is {second}");
+        ReadLine();
+    }
+
+    public static void InputMethod(out int first, out int second)
+    {
+        first = DataEntry("first");
+        second = DataEntry("second");
+    }
+
+    public static int DataEntry(string whichOne)
+    {
+        Write($"Enter {whichOne} integer: ");
+        if (!int.TryParse(ReadLine(), out int data))
+        {
+            WriteLine("Invalid input. Please try again.");
+            return DataEntry(whichOne);
+        }
+        return data;
+    }
+}
